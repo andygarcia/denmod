50cf98c02fd51923b9742baefa86f51f038bbe38
Andy Garica <andygarcia@gmail.com>
2010-05-03 16:37:53 -0400

    - Removed reference to deleted discrete simulation header.

--- trunk/gui/objs/Location.h
+++ trunk/gui/objs/Location.h
@@ -16,7 +16,6 @@
 #pragma managed(push, off)
 #include "../../simulation/input/Location.h"
 #include "../../simulation/cimsim/Simulation.h"
-#include "../../simulation/cimsim/DiscreteSimulation.h"
 #include "../../simulation/dsport/Simulation.h"
 #include "../../simulation/output/CimsimOutput.h"
 #include "../../simulation/output/DensimOutput.h"
